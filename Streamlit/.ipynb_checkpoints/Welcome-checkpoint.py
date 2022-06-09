70b84642eee70a39d0b60929c98ac6f78da2f444
Tamara Frances <tamarafrances@Tamaras-Air.lan>
2022-06-08 22:55:32 -0400

    added requirements.txt

---
+++ Streamlit/.ipynb_checkpoints/Welcome-checkpoint.py
@@ -0,0 +1,42 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+
+
+
+
+
+st.title('Discord Spam Detector 👾')
+st.subheader('Tamara Frances')
+import numpy as np
+import time
+import pickle
+st.markdown("""---""")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.write('Summary will be written here')
+
+with col2:
+    with open("saved_model.pkl", "rb") as f:
+        trained_classifier = pickle.load(f)
+
+    import numpy as np
+
+
+    def predict_spam(trained_classifier, text_input):
+        return trained_classifier.predict([text_input])
+
+    st.header('Test Your Message')
+
+    with st.form(key='my_form'):
+        text_input = st.text_input(label='Enter your message below')
+        submit_button = st.form_submit_button(label='Predict')
+        if text_input != '':
+            with st.spinner("Predicting..."):
+                time.sleep(2)
+                prediction = predict_spam(trained_classifier, text_input)
+                if prediction == 'N':
+                    st.write('Your message was not flagged as spam!')
+                    st.balloons()
+                else:
+                    st.subheader('FLAGGED AS SPAM')
